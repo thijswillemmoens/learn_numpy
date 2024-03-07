ccd5a99e015696155f6b23bcdece0e7fa5cba8ad
Thijs Moens <thijs@raketwetenschap.com>
2024-03-07 16:09:57 +0100

    Update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy
+import numpy as np
 
-arr = numpy.array([1, 2, 3, 4, 5])
+arr = np.array([1, 2, 3, 4, 5])
 
-print(arr)
+threed_arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(threed_arr)
